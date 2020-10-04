a92074e85f842a06a18d334cfe22ab5c934731c1
ElK <62822011+evkonradi@users.noreply.github.com>
2020-10-04 16:30:31 -0500

    portfolio page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
+import { validateEmail } from '../../utils/helpers';
 
 function Contact() {
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } 
+        else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value })
+        }
+
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
+
     return (
-        <div>
-            
-        </div>
+        <section className="section-container">
+            <div className="section-left">
+                <h2>Contact me</h2>
+            </div>     
+            <div className="section-right">
+                <form id="contact-form" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" name="name" defaultValue={name} onBlur={handleChange}/>
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email address:</label>
+                        <input type="email" name="email" defaultValue={email} onBlur={handleChange}/>
+                    </div>
+                    <div>
+                        <label htmlFor="message">Message:</label>
+                        <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+                    </div>
+                    {errorMessage && (
+                        <div>
+                            <p className="error-text">{errorMessage}</p>
+                        </div>
+                    )}
+                    <button type="submit">Submit</button>
+                </form>
+            </div>       
+
+        </section>
     )
 }
 
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Portfolio() {
+
+    const [projects] = useState([
+        {
+            fileName : './assets/projects/cardsocial.png',
+            title: 'CardSocial',
+            technology: 'Node.js/Express/MySQL/Sequelize/JavaScript',
+            githubLink: 'https://github.com/evkonradi/CardSocial',
+            deployedLink: ''
+        },
+        {
+            fileName : './assets/projects/project3.jpg',
+            title: 'Calculator',
+            technology: 'React/JavaScript/CSS',
+            githubLink: '',
+            deployedLink: ''
+        },
+        {
+            fileName : './assets/projects/project_4.png',
+            title: 'Pastel Puzzels',
+            technology: 'MERN Stack',
+            githubLink: '',
+            deployedLink: ''
+        },
+        {
+            fileName : './assets/projects/project5.jpg',
+            title: 'Run Buddy',
+            technology: 'HTML/CSS',
+            githubLink: '',
+            deployedLink: ''
+        }
+    ])
+
     return (
-        <div>
-            
-        </div>
+        <section id="projects" className="section-container">
+            <div className="section-left">
+                <h2>Portfolio</h2>
+                <p className="about-me-p">
+                    <br></br>
+                    Below I introduce some of my projects. Please click the links for each project to proceed to the GitHub repository and deployed application.
+                    <br></br>
+            </p>
+            </div>
+            <div className="section-right work-grid">
+                <article className="work-grid-item work-grid-item-1">
+                    <div className="card work-card-flex">
+                        <a href = "https://github.com/pochobs/project1" className="work-card-link">
+                            <p className="work-card-title">Rentz rentals</p>
+                            <p className="work-card-subtitle">Server APIs/JavaScript/CSS Foundation</p>
+                        </a>
+                    </div>
+                </article>
+                {projects.map((project, i) => (
+                        <article key={project.title} className={`work-grid-item work-grid-item-${i+2}`}>
+                            <div className="card work-card-flex">
+                                <a className="work-card-link" href ={`${project.githubLink}`}  >
+                                    <p className="work-card-title">{project.title}</p>
+                                    <p className="work-card-subtitle">{project.technology}</p>
+                                </a>
+                            </div>
+                        </article>
+            ))}
+            </div>
+
+        </section>
     )
 }
 
