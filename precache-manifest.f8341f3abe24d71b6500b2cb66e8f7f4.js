1537519f5d24ea711803bfa34eaeb078157dc80a
ElK <62822011+evkonradi@users.noreply.github.com>
2020-10-04 22:28:49 -0500

    Updates

---
+++ precache-manifest.f8341f3abe24d71b6500b2cb66e8f7f4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b1876b332fb7d911393aa6bea2df65",
+    "url": "/portfolio-ek/index.html"
+  },
+  {
+    "revision": "40680fd27f08854c77ec",
+    "url": "/portfolio-ek/static/css/main.d91a5aa6.chunk.css"
+  },
+  {
+    "revision": "744525c82b1be3535492",
+    "url": "/portfolio-ek/static/js/2.54abe2c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-ek/static/js/2.54abe2c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40680fd27f08854c77ec",
+    "url": "/portfolio-ek/static/js/main.d636df1d.chunk.js"
+  },
+  {
+    "revision": "de12be8ac7755d7941d5",
+    "url": "/portfolio-ek/static/js/runtime-main.1c4c27e0.js"
+  },
+  {
+    "revision": "1c1fa89acf8b68cd92d1c5dc9f411e64",
+    "url": "/portfolio-ek/static/media/Resume_EK.1c1fa89a.pdf"
+  },
+  {
+    "revision": "4d8b0842fcdd3d98daaeaffa338e9a7b",
+    "url": "/portfolio-ek/static/media/badget-tracker.4d8b0842.jpg"
+  },
+  {
+    "revision": "baffd0b21c1607969864af65d6bdaea3",
+    "url": "/portfolio-ek/static/media/banner-clouds.baffd0b2.jpg"
+  },
+  {
+    "revision": "0d8e45d26e5709c97f543a47c8c14aaf",
+    "url": "/portfolio-ek/static/media/cardsocial.0d8e45d2.png"
+  },
+  {
+    "revision": "faef2c3c8c4a7483a2ac789a464a0557",
+    "url": "/portfolio-ek/static/media/e-commerce-APIs.faef2c3c.jpg"
+  },
+  {
+    "revision": "baf5704791515dc16820abbe58615b77",
+    "url": "/portfolio-ek/static/media/email.baf57047.png"
+  },
+  {
+    "revision": "fd17c9abf0e3bb2a2fac70528670d417",
+    "url": "/portfolio-ek/static/media/github.fd17c9ab.png"
+  },
+  {
+    "revision": "6262972254e9ee3e158ab17049d22ad6",
+    "url": "/portfolio-ek/static/media/linkedin.62629722.png"
+  },
+  {
+    "revision": "2d3b7908a4cec7379a856a0daf10339f",
+    "url": "/portfolio-ek/static/media/rentzsite.2d3b7908.jpg"
+  },
+  {
+    "revision": "231a12094d6e4d0effd697b91dc58ecc",
+    "url": "/portfolio-ek/static/media/tech-blog.231a1209.jpg"
+  },
+  {
+    "revision": "b9618806cd79482e58d4de81fe1fdcd7",
+    "url": "/portfolio-ek/static/media/weather-dashboard.b9618806.jpg"
+  }
+]);
